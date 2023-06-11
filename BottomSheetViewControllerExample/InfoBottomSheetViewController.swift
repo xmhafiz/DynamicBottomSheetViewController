1723cb85d1eb55f712a011d585f5a1b16507d380
Mohd Hafiz <xmhafiz@gmail.com>
2023-06-11 08:44:51 +0800

    init project

---
+++ BottomSheetViewControllerExample/InfoBottomSheetViewController.swift
@@ -0,0 +1,12 @@
+//
+//  InfoBottomSheetViewController.swift
+//  BottomSheetViewControllerExample
+//
+//  Created by Mohd Hafiz on 04/06/2023.
+//
+
+import UIKit
+
+class InfoBottomSheetViewController: BottomSheetViewController {
+    
+}
---
+++ BottomSheetViewControllerExample/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController.swift
+//  BottomSheetViewControllerExample
+//
+//  Created by Mohd Hafiz on 04/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+}
